86ce9aebf9fdb05ef6d68ffe3a22e5e5b40aa597
JanAlexanderZak <janalexzak@gmail.com>
2020-12-14 12:49:55 +0100

    add: staticfiles

--- janzak/settings.py
+++ janzak/settings.py
@@ -134,5 +134,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
